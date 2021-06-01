422ac48cb74c63ea9dc7e693dd5a76ec5686b7aa
Sandy A <sagarwal9449@gmail.com>
2021-06-01 11:42:09 +0530

    comment removed

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const fetch = require('node-fetch');
-//console.log("Hello Medha");
-
 var inputs = process.argv;
-//console.log(inputs[2]);
 var month = inputs[2];
 var date = inputs[3];
-
 
 var url = "https://history.muffinlabs.com/date/" + month + "/" + date;
 console.log("URL is: " + url);
@@ -14,7 +10,6 @@ fetch(url)
     .then(res => res.text())
     .then(data => {
           var obj = JSON.parse(data);
-          //alert(obj.data.Events[0].year);
           var events = obj.data.Events;
           events.forEach((item) => {
             console.log(item.year + ": " + item.text);
@@ -33,7 +28,5 @@ fetch(url)
           //document.getElementById('APIResp').innerText = data;
         }*/
 
-/*const prompt = require('prompt-sync')();
 
-const name = prompt('What is your name?');
-console.log(`Hey there ${name}`);*/
+
